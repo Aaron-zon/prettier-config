c63fa0f2c69cfb6848e5a10ba673b51d3057bc1a
dl_zhoujunyang <1640485761@qq.com>
2023-09-28 17:34:53 +0800

    aaron-zon

---
+++ index.js
@@ -0,0 +1,20 @@
+module.exports = {
+    /** 单行最大文字数*/
+    printWidth: 80,
+    /** tab填充空格数 */
+    tabWidth: 4,
+
+    /** 结尾是否加分号 */
+    semi: true,
+    /** 使用单引号 */
+    singleQuote: true,
+    /** jsx使用单引号 */
+    jsxSingleQuote: true,
+
+    /** 对象中括号与文字之间填充空格 */
+    bracketSpacing: true,
+    /** 标签多行时结尾>换行 */
+    bracketSameLine: true,
+    /** 箭头函数 参数在唯一时也包含在括号中 */
+    arrowParens: "always",
+}
